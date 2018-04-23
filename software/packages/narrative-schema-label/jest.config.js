1e46a328bbe902aef9d6bcd6d5ee7acb101d11ad
Alexander Kachkaev <alexander@kachkaev.ru>
2018-04-23 08:40:45 +0100

    Simplify TypeScript and Jest configs

--- software/packages/narrative-schema-label/jest.config.js
+++ software/packages/narrative-schema-label/jest.config.js
@@ -1,10 +1,6 @@
+const jestBase = require("../../jest.config.js");
+
 module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.{ts,tsx,js}"],
-  moduleFileExtensions: ["ts", "tsx", "js"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
-  rootDir: "src",
-  testMatch: ["**/?(*.)(spec|test).(j|t)s?(x)"],
+  ...jestBase,
+  rootDir: ".",
 };
